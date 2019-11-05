c54a3d1cecd267d588ee290d1b68d1129684cbec
Sean Leif Hellier <sean@newtropolis.com>
2019-11-05 13:06:08 -0500

    added .env, zomato.js

---
+++ public/js/zomato.js
@@ -0,0 +1,48 @@
+// 16781992
+// 16784984
+
+// alert("it's connected");
+console.log("it's connected");
+
+var x = document.getElementById('demo');
+
+const config = { headers: { 'user-key': 'bf0b007a082354a7c35efef48bf5a3c9' } };
+
+function zomatoAPICall(lat, lng) {
+	axios.get(`https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lng}`, config).then((response) => {
+		console.log(response);
+
+		var restArr = [];
+
+		var restaurantList = response.data.nearby_restaurants;
+
+		for (var i = 0; i < restaurantList.length; i++) {
+			restArr.push(restaurantList[i]);
+		}
+
+		console.log(restArr);
+		var rest_id = '16784984';
+		ajax.get('/api/examples/' + rest_id).then((res) => {
+			console.log(res);
+		});
+	});
+}
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = 'Geolocation is not supported by this browser.';
+	}
+}
+
+function showPosition(position) {
+	x.innerHTML = 'Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
+	console.log(position.coords.latitude);
+	console.log(position.coords.longitude);
+	var pos = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude
+	};
+	zomatoAPICall(pos.lat, pos.lng);
+}
